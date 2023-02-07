c7fada650168208e5531e219fa5796f4d4754ab4
Charlotte Fouques <charlottefouques10@gmail.com>
2023-02-07 14:31:46 +0100

    DOVENT

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const userCtrl = require('../controllers/user')
+const limiter = require('../middleware/limiter')
 
 const router = express.Router();
-
+router.use(limiter)
 router.post('/signup', userCtrl.signup)
 router.post('/login', userCtrl.login)
 
